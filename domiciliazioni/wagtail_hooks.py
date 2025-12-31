d4d8c0ae8308fe93a941c96eb1fcf3b58ba3b77b
Bertalan Ivan <bertalan.ivan@gmail.com>
2025-12-31 14:57:32 +0100

    Fix Wagtail menu: use Menu(items=[]) pattern like booking app

--- domiciliazioni/wagtail_hooks.py
+++ domiciliazioni/wagtail_hooks.py
@@ -7,34 +7,22 @@ from wagtail.admin.menu import MenuItem, Menu, SubmenuMenuItem
 from .models import DomiciliazioniSubmission
 
 
-class DomiciliazioniMenu(Menu):
-    """Menu per gestione domiciliazioni."""
-    pass
-
-
 @hooks.register('register_admin_menu_item')
 def register_domiciliazioni_menu():
     """Registra il menu Domiciliazioni nel pannello admin."""
     
-    submenu = DomiciliazioniMenu(
-        register_hook_name='register_domiciliazioni_menu_item',
-        construct_hook_name='construct_domiciliazioni_menu'
-    )
+    domiciliazioni_menu = Menu(items=[
+        MenuItem(
+            'Richieste',
+            '/admin/snippets/domiciliazioni/domiciliazionisubmission/',
+            icon_name='list-ul',
+            order=100
+        ),
+    ])
     
     return SubmenuMenuItem(
         'Domiciliazioni',
-        submenu,
+        domiciliazioni_menu,
         icon_name='doc-full',
         order=300
-    )
-
-
-@hooks.register('register_domiciliazioni_menu_item')
-def register_domiciliazioni_submenu_items():
-    """Registra le voci del sottomenu domiciliazioni."""
-    return MenuItem(
-        'Richieste',
-        '/admin/snippets/domiciliazioni/domiciliazionisubmission/',
-        icon_name='list-ul',
-        order=100
     )
