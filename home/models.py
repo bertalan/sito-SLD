567a653b144163f83162140c1625c36bfc39387b
Bertalan Ivan <bertalan.ivan@gmail.com>
2026-01-01 15:40:15 +0100

    Hero: 4 campi separati (righe 1-2 nere, righe 3-4 magenta)

--- home/models.py
+++ home/models.py
@@ -7,8 +7,11 @@ from wagtail.admin.panels import FieldPanel, MultiFieldPanel
 class HomePage(Page):
     """Homepage Studio Legale D'Onofrio."""
     
-    # Hero
-    hero_title = models.TextField("Titolo Hero", default="ASSISTENZA LEGALE\nPER UNA TUTELA\nDI ELEVATA EFFICACIA\nE COMPETENZA")
+    # Hero - 4 righe
+    hero_line1 = models.CharField("Riga 1 (nera)", max_length=100, default="ASSISTENZA LEGALE")
+    hero_line2 = models.CharField("Riga 2 (nera)", max_length=100, default="PER UNA TUTELA")
+    hero_line3 = models.CharField("Riga 3 (magenta)", max_length=100, default="DI ELEVATA EFFICACIA")
+    hero_line4 = models.CharField("Riga 4 (magenta)", max_length=100, default="E COMPETENZA")
     hero_subtitle = models.CharField("Sottotitolo", max_length=200, default="D'ONOFRIO")
     hero_accent = models.CharField("Testo accent", max_length=100, default="GIUSTIZIA.")
     hero_description = RichTextField("Descrizione", blank=True)
@@ -30,7 +33,10 @@ class HomePage(Page):
     
     content_panels = Page.content_panels + [
         MultiFieldPanel([
-            FieldPanel('hero_title'),
+            FieldPanel('hero_line1'),
+            FieldPanel('hero_line2'),
+            FieldPanel('hero_line3'),
+            FieldPanel('hero_line4'),
             FieldPanel('hero_subtitle'),
             FieldPanel('hero_accent'),
             FieldPanel('hero_description'),
